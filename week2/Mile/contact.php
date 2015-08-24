3a147b98bed4bae2213dfbad7a97912a598d5f3c
Sean <seanpeter.aguirre@gmail.com>
2015-08-24 14:54:32 -0700

    new

---
+++ week2/Mile/contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<title>Contact</title>
+</head>
+<body>
+	<?php require('header.php'); ?>
+
+	<h1>Contact</h1>
+	<p>Phone (480)555-5555</p>
+	<p>Email: Blah@blah.com</p>
+
+	<?php require('footer.php'); ?>
+</body>
+</html>
