9060ba6ed3b32ec89ce14fe7f8d24877161bf725
Sean <seanpeter.aguirre@gmail.com>
2015-08-24 14:44:46 -0700

    update

---
+++ week2/Milestone/browse.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="style.css">
+	<title>Browse</title>
+</head>
+<body>
+	<?php require('header.php'); ?>
+
+	<h1>Browse Products</h1>
+	<a class="link" href="product.php?id=1">Macbook<br></a>
+	<a class="link" href="product.php?id=2">Ubuntu<br></a>
+	<a class="link" href="product.php?id=3">Windows<br></a>
+
+	<?php require('footer.php'); ?>
+</body>
+</html>
---
+++ week2/Milestone/header.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="style.css">
+
+<div class='header'>
+	<p class="business">Business Site</p>
+	<div>
+		<a class="link" href="index.php">Home</a> |
+		<a class="link" href="contact.php">Contact</a> |
+		<a class="link" href="browse.php">Browse Products</a> 
+	</div>
+</div>
