2d7d439014a3e662de6c8a97d79333f608628049
Sean <seanpeter.aguirre@gmail.com>
2015-09-17 10:29:43 -0700

    elephant

--- week4/Mile2/account.php
+++ week4/Mile2/account.php
@@ -9,7 +9,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 }
 
-$msg3 = "Hello " . $_SESSION["user"] . " you are logged in";
+$msg3 = "Hello " . $_SESSION["user"] . " you are being logged in";
 
 ?>
 
@@ -17,7 +17,33 @@ $msg3 = "Hello " . $_SESSION["user"] . " you are logged in";
 <link href='https://fonts.googleapis.com/css?family=Indie+Flower|Amatic+SC|Bangers|Fredericka+the+Great|Black+Ops+One' rel='stylesheet' type='text/css'>
 
 <div class="title">Welcome to Forrest Hunter</div>
-
+<!-- Elephant -->
+<div class="contenedor">
+<div class="elefantes">
+<div class="elefante">
+  <div class="cabeza">
+    <div class="ojo"></div>
+    <div class="trompa"></div>
+  </div>  
+  <div class="body"></div>
+    <div class="legs"><div class="legs3"></div></div>
+  <div class="orejas"></div>
+  <div class="cola"></div>
+</div>
+<div class="elefante3">
+  <div class="cabeza3">
+    <div class="ojo3"></div>
+    <div class="trompa3"></div>
+  </div>  
+  <div class="body3"></div>
+    <div class="legs5"><div class="legs7"></div></div>
+  <div class="orejas3"></div>
+  <div class="cola3"></div>
+</div>
+  <div class="sol"></div>
+</div>
+  </div>
+<!-- Elephant -->
 <div class="account">
 	<div class="greet">
 		<?= $msg3 ?>
