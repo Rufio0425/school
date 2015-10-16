e5ef713cd824ce54ea42a0513f4dbe79407576a4
Sean <seanpeter.aguirre@gmail.com>
2015-10-15 18:02:23 -0700

    bower and gulp update

--- skate/resources/views/welcome.blade.php
+++ skate/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>Volunteers</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:s100" rel="stylesheet" type="text/css">
 
         <link href="" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="fonts/">
@@ -35,11 +35,9 @@
                 width: 100%;
                 display: table;
                 font-weight: 100;
-                font-family: 'Lato';
-                color:#FFFFFF;
             }
 
-            .container {
+           /* .container {
                 background-color: orange;
                 text-align: center;
                 display: inline-block;
@@ -57,28 +55,121 @@
             .title {
                 font-size: 200px;
                 font-family:'LeagueGothicRegular';
-            }
+            }*/
+            
 
-            nav{
-                padding:10px;
-                text-align:right;
+            header{
+                display:flex;
+                text-align:center;
                 background-color: black;
+                font-family:helvetica;
+                position:fixed;
+                width:100%;
+                z-index:1000;
             }
 
-            nav a{
-                font-family:helvetica;
-                color:white;
+            .logo{
+                text-align:left;
+                background-color: orange;
+                flex:1;
+                padding:15px 70px;
+            }
+
+            .nav{
+                background-color: red;
+                flex:3;
+                text-align:right;
+                padding:15px 51px 15px;
+            }
+
+            .nav a{
+                background-color: white;
+                font-size:10pt;
+                padding:10px;
+            }
+
+            .hero{
+                padding-top:33px;
+                width:100%;
+                background-color: blue;
+            }
+
+            .main{
+                margin-top:15px;
+                color:black;
+                width:74em;
+                margin-left:auto;
+                margin-right:auto;
+                background-color: yellow;
+            }
+
+            .title{
+                font-size:120px;
+                text-align:center;
+                background-color: green;
+            }
+
+            .title span{
+                background-color: pink;
+            }
+
+            .vom{
+                text-align:center;
+                background-color: brown;
+            }
+
+            .content{
+                width:74em;
+                margin-left:auto;
+                margin-right:auto;
+            }
+
+            .vom_title{
+                padding:10px;
+                background-color: orange;
+            }
+
+            .vom_title span{
+                font-size:50px;
+                background-color: yellow;
+            }
+
+            .vol_name{
+                font-size:25px;
             }
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="logo"></div>
-            <nav>
+        <header>
+            <div class="logo"><a href="">Skate After School</a></div>
+            <div class="nav">
                 <a href="">Home</a>
-                <a href="">Volunteers</a>
-                <a href="">Signup</a>
-            </nav>
+                <a href="">About</a>
+                <a href="">Who's Helping</a>
+                <a href="">Team</a>
+                <a href="">Give</a>
+                <a href="">Contact</a>
+                <a href="">Volunteer Application</a>
+                <a href="">Donate</a>
+                <a href="">Store</a>
+            </div>
+        </header>
+        <div class="panel hero">
+            <div class="main">
+                <div class="banner">
+                    <div class="title"><span>Volunteers</span></div>
+                </div>
+                <div class="vom">
+                    <div class="vom_title"><span>Volunteer of The Month</span></div>
+                    <div class="profile_pic"><img src="http://assets.woodwardtv.com/media/thumbs/athlete/headshot/96157931_RYAN_LAY_HEADSHOT_.jpg.360x360_q85_crop-scale_upscale.jpg"></div>
+                    <div class="vol_name"><a href="">Ryan Lay</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="panel content">
+            <div class="volunteers">
+                div.profile*6>
+            </div>
         </div>
     </body>
 </html>
---
+++ week9/thur/bower/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('evan', function(){
+	console.log('Hi from evan!!!');
+});
+
+gulp.task('thursday', function(){
+	console.log('Its thursday');
+});
